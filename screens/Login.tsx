52fdf614a5abe2dcb8c8afcf17b6e7c119806b16
jason-warner <thuperman@protonmail.com>
2021-09-12 19:45:32 -0400

    login screen progress

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,22 +1,73 @@
 import React from 'react';
+import { View } from 'react-native';
+import { Formik } from 'formik';
+import { Octicons } from '@expo/vector-icons';
+import { StatusBar } from 'expo-status-bar';
 
+// import { Colors } from 'react-native/Libraries/NewAppScreen';
 import {
     StyledContainer,
     InnerContainer,
     PageLogo,
-    PageTitle
+    PageTitle,
+    SubTitle,
+    StyledFormArea,
+    LeftIcon,
+    StyledInputLabel,
+    StyledTextInput,
+    RightIcon,
+    Colors
 } from './../components/styles';
 
+const { brand, darkLight } = Colors;
 
 const Login = () => {
     return (
         <StyledContainer>
+            <StatusBar style="dark" />
             <InnerContainer>
                 <PageLogo source={require('./../assets/logo.png')} resizeMode="cover" />
                 <PageTitle> TempLogin </PageTitle>
+                <SubTitle> Account Login </SubTitle>
+
+                <Formik
+                    initialValues={{ email: '', passowrd: '' }}
+                    onSubmit={(values) => { console.log(values) }}
+                >
+                    {({ handleChange, handleBlur, handleSubmit, values }) => (
+                        <StyledFormArea>
+                            <MyTextInput
+                                label="Email Address"
+                                icon="mail"
+                                placeholder="jdoe@mail.com"
+                                placeholderTextColor={darkLight}
+                                onChangeText={handleChange('email')}
+                                onBlur={handleChange('email')}
+                                value={values.email}
+                                keyboardType="email-address"
+                            />
+                        </StyledFormArea>
+                    )}
+                </Formik>
             </InnerContainer>
         </StyledContainer>
     );
+}
+
+const MyTextInput = ({ label, icon, ...props }: {
+    [x: string]: any;
+    label: string;
+    icon: any;
+}) => {
+    return (
+        <View>
+            <LeftIcon>
+                <Octicons name={icon} size={30} />
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+            <StyledTextInput {...props} />
+        </View>
+    )
 }
 
 export default Login;
